0abab30faafa109b4436e393810a9f1c01baaaff
Foluso Ogunfile <folusoogunfile@yahoo.com>
2020-01-20 19:37:20 +0100

    local time versus heroku time test

--- controller/prayer.js
+++ controller/prayer.js
@@ -5,7 +5,7 @@ const Worker = require("../models/worker")
 
 function lastMondayfunction(msDate) {
     // let refSunday = 318600000; //Sun, 4th January 1970, 17:30:000;
-    let refMonday = 318600000 + (13 * 1800 * 1000); //Mon, 5th January 1970, 00:00:000;
+    let refMonday = 318600000 + (13 * 1800 * 1000) + (3600 * 1000); //Mon, 5th January 1970, 01:00:000;
     let week = 604800000; // Number of milliseconds in a week;
     let timeAYear = 31536000000; // Number of milliseconds in a year;
     let diffTime = msDate - refMonday;
